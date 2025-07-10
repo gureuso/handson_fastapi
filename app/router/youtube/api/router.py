225b72770babf3cd6a7a510f8c40f26d940da6db
윤정현 <wyun13043@gmail.com>
2025-07-10 09:19:37 +0900

    Fix

--- app/router/youtube/api/router.py
+++ app/router/youtube/api/router.py
@@ -94,36 +94,6 @@ async def signout():
     return response
 
 
-@router.get('/token')
-async def token(request: Request):
-    access_token = request.cookies.get('x-access-token')
-
-    response = RedirectResponse(
-        url='http://localhost:3000' if Config.APP_MODE == Config.APP_MODE_DEVELOPMENT else 'https://youtube.devmaker.kr',
-        status_code=status.HTTP_302_FOUND)
-
-    try:
-        jwt.decode(access_token, Config.SECRET, algorithms=['HS256'])
-    except jwt.exceptions.ExpiredSignatureError:
-        exp_token = await Cache.get(f'fastapi_jwt_token_{access_token}')
-        if exp_token:
-            return response
-        else:
-            await Cache.set(f'fastapi_jwt_token_{access_token}', access_token)
-
-            jwt_data = jwt.decode(access_token, Config.SECRET, algorithms=['HS256'], options={'verify_exp': False})
-            new_token = jwt.encode({'id': jwt_data['id'], 'exp': datetime.utcnow() + timedelta(hours=1)},
-                                   JsonConfig.get_data('SECRET'), algorithm='HS256')
-
-            if Config.APP_MODE == Config.APP_MODE_DEVELOPMENT:
-                response.set_cookie('x-access-token', new_token, httponly=True)
-            else:
-                response.set_cookie('x-access-token', new_token, domain='youtube.devmaker.kr', httponly=True,
-                                    secure=True)
-
-    return response
-
-
 @router.get('/callback/{provider}')
 async def callback(provider: Literal['google', 'facebook', 'kakao', 'naver', 'github'], code: str):
     sns_info = SNSInfo(provider, code)
@@ -151,16 +121,13 @@ async def callback(provider: Literal['google', 'facebook', 'kakao', 'naver', 'gi
             nickname=nickname,
         ))
 
-    response = RedirectResponse(url='http://localhost:3000' if Config.APP_MODE == Config.APP_MODE_DEVELOPMENT else 'https://youtube.devmaker.kr', status_code=status.HTTP_302_FOUND)
-
+    response = RedirectResponse(
+        url='http://localhost:3000' if Config.APP_MODE == Config.APP_MODE_DEVELOPMENT else 'https://youtube.devmaker.kr',
+        status_code=status.HTTP_302_FOUND
+    )
+    new_token = jwt.encode({'id': user.id, 'exp': datetime.utcnow() + timedelta(hours=1)}, JsonConfig.get_data('SECRET'), algorithm='HS256')
     if Config.APP_MODE == Config.APP_MODE_DEVELOPMENT:
-        response.set_cookie('x-access-token',
-                            jwt.encode({'id': user.id, 'exp': datetime.utcnow() + timedelta(hours=1)}, JsonConfig.get_data('SECRET'), algorithm='HS256'),
-                            httponly=True)
+        response.set_cookie('x-access-token', new_token, httponly=True)
     else:
-        response.set_cookie('x-access-token',
-                            jwt.encode({'id': user.id, 'exp': datetime.utcnow() + timedelta(hours=1)}, JsonConfig.get_data('SECRET'), algorithm='HS256'),
-                            domain='youtube.devmaker.kr',
-                            httponly=True,
-                            secure=True)
+        response.set_cookie('x-access-token', new_token, domain='youtube.devmaker.kr', httponly=True, secure=True)
     return response
