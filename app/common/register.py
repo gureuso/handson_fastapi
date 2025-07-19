7903d0f42c58805da8cf27d7cec816125a9908ee
윤정현 <alex_yun@yunjeonghyeon-ui-MacBookAir.local>
2025-07-19 14:54:55 +0900

    Change method to private method for APIRouterRegister

--- app/common/register.py
+++ app/common/register.py
@@ -2,10 +2,11 @@
 import importlib
 import os
 import re
+from fastapi import FastAPI
 
 
 class APIRouterRegister:
-    def __init__(self, app, module_path, controller_name):
+    def __init__(self, app: FastAPI, module_path: str, controller_name: str):
         self.app = app
         self.module_path = module_path
         self.controller_name = controller_name
@@ -13,30 +14,30 @@ class APIRouterRegister:
         self.directories = []
 
     def register(self):
-        self.find_dir(self.controller_path)
+        self._find_dir(self.controller_path)
         for dir_path in self.directories:
             dir_path = dir_path[1:].replace('/', '.').replace('\\', '.')
             module_path = '{}.{}.{}'.format(self.module_path, dir_path, self.controller_name)
             module = importlib.import_module(module_path)
             self.app.include_router(module.router)
 
-    def find_dir(self, path):
+    def _find_dir(self, path):
         files = os.listdir(path)
         for file_name in files:
             isdir = os.path.isdir(os.path.join(*[path, file_name]))
             if not isdir:
                 continue
-            ignore = self.ignore(file_name)
+            ignore = self._ignore(file_name)
             if ignore:
                 continue
-            self.append_dir(path, file_name)
+            self._append_dir(path, file_name)
 
-    def append_dir(self, path, file_name):
+    def _append_dir(self, path, file_name):
         dir_path = os.path.join(*[path, file_name])
         self.directories.append(dir_path.replace(self.controller_path, ''))
-        self.find_dir(dir_path)
+        self._find_dir(dir_path)
 
-    def ignore(self, name):
+    def _ignore(self, name):
         # __pycache__/
         if re.match('__.*__', name):
             return True
